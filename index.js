e76d19397b380893a33279cb4a218b13aaa6775c
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-05-21 01:42:45 +0300

    ready for development

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {View, AppRegistry} from 'react-native'
+import Header from './src/components/Header'
+
+const App = () => {
+  return <Header />
+}
+AppRegistry.registerComponent('sampleProject', () => App)
