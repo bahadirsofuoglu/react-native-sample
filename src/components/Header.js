ff70b9586ae7f2a37f40cd0d89884bdfaa54d974
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-05-21 01:43:10 +0300

    Header component created

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+const Header = () => {
+  const {textStyling, viewStyle} = styles
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyling}> Cars </Text>
+    </View>
+  )
+}
+
+const styles = {
+  textStyling: {
+    fontSize: 20,
+  },
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 35,
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+  },
+}
+
+export default Header
