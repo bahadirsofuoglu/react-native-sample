687a4d768a6365834e6af7cd2b43cfa32c8b9b3e
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-05-21 11:43:13 +0300

    about CarList items render

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -2,17 +2,24 @@ import React, {Component} from 'react'
 import {View, Text} from 'react-native'
 import axios from 'axios'
 class CarList extends Component {
+  constructor () {
+    super()
+
+    this.state = {carList: []}
+  }
   componentDidMount () {
     axios.get('https://givecars.herokuapp.com').then(res => {
-      console.log(res.data)
+      this.setState({carList: res.data})
     })
   }
+
+  renderList = () => {
+    return this.state.carList.map(brand => (
+      <Text key={brand.model[0].name}>{brand.model[0].name}</Text>
+    ))
+  }
   render () {
-    return (
-      <View>
-        <Text>Car List</Text>
-      </View>
-    )
+    return <View>{this.renderList()}</View>
   }
 }
 
