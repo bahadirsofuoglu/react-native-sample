8964bc0f39200c829bf494e2ac9984a8a0577c54
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-05-21 11:08:15 +0300

    axios added. Cars data got from http

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react'
 import {View, Text} from 'react-native'
-
+import axios from 'axios'
 class CarList extends Component {
   componentDidMount () {
-    console.log('hello')
+    axios.get('https://givecars.herokuapp.com').then(res => {
+      console.log(res.data)
+    })
   }
   render () {
     return (
